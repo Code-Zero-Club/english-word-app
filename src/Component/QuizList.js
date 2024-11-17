281fe9dd0592284bafbc2e96d5580d92e8a0e9ec
Limhobin <gyh2023066@h.jne.go.kr>
2024-11-18 07:53:20 +0900

    페이지 이름 변경

--- src/Component/QuizList.js
+++ src/Component/QuizList.js
@@ -6,8 +6,8 @@ function QuizList(){
             <h1 className="page-title">Quiz</h1>
             <nav className="navigation">
                 <ul className="nav-list">
-                    <li className="nav-item"><Link to="/" className="nav-link">홈</Link></li>
-                    <li className="nav-item"><Link to="/vocabularyList" className="nav-link">단어</Link></li>
+                    <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                    <li className="nav-item"><Link to="/vocabularyList" className="nav-link">Word</Link></li>
                     <li className="nav-item"><Link to="/quiz" className="nav-link">Quiz</Link></li>
                 </ul>
             </nav>
