7d8b8f81d70120b1bb2d0f3d4ddc7c4898ead82f
Limhobin <gyh2023066@h.jne.go.kr>
2024-11-18 07:53:12 +0900

    페이지 이름 변경

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -6,8 +6,8 @@ function Home() {
         <h1 className="page-title" >CZWA</h1>
         <nav className="navigation">
           <ul className="nav-list">
-            <li className="nav-item"><Link to="/" className="nav-link">홈</Link></li>
-            <li className="nav-item"><Link to="/vocabularyList" className="nav-link">단어</Link></li>
+            <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+            <li className="nav-item"><Link to="/vocabularyList" className="nav-link">Word</Link></li>
             <li className="nav-item"><Link to="/quizList" className="nav-link">Quiz</Link></li>
           </ul>
         </nav>
