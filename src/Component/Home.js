d1393753b734ea879c2122d9abdb39c7e8623e65
Limhobin <gyh2023066@h.jne.go.kr>
2024-11-19 07:59:05 +0900

    Home 페이지에 링크 추가

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -13,8 +13,18 @@ function Home() {
         </nav>
         <p className="welcome-message">단어 학습 웹에 오신 것을 환영합니다! 상단의 링크를 통해 다양한 기능을 이용해보세요.</p>
         <li className="nav-item"><Link to="/Quiz" className="nav-link-word">Home</Link></li>
-        <li className="nav-item"><Link to="/Quiz2" className="nav-link-word">Word</Link></li>
-        <li className="nav-item"><Link to="/Quiz2" className="nav-link-word">QUiz</Link></li>
+        <li className="nav-item"><Link to="/Quiz2" className="nav-link-word">Word</Link>
+          <ol>
+            <li className="nav-item"><Link to="/Vocabulary" className="nav-link-word">Word - 1세트</Link></li>
+            <li className="nav-item"><Link to="/Vocabulary2" className="nav-link-word">Word - 2세트</Link></li>
+          </ol>
+        </li>
+        <li className="nav-item"><Link to="/Quiz2" className="nav-link-word">Quiz</Link>
+          <ol>
+              <li className="nav-item"><Link to="/Quiz" className="nav-link-word">Quiz - 1세트</Link></li>
+              <li className="nav-item"><Link to="/Quiz2" className="nav-link-word">Quiz - 2세트</Link></li>
+          </ol>
+        </li>
       </div>
     );
   }
